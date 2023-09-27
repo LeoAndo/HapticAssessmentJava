62ba277aa510b72a4a92f29d30f1e172ec70ca88
ando <leo81@outlook.jp>
2023-09-27 21:59:07 +0900

    update

--- app/src/main/java/com/leoleo/hapticassessment/java/MainActivity.java
+++ app/src/main/java/com/leoleo/hapticassessment/java/MainActivity.java
@@ -2,13 +2,48 @@ package com.leoleo.hapticassessment.java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private static final long ONE_SHOT_TIMING = 20L;
+    private static final int ONE_SHOT_AMPLITUDE = 255;
+    private static final long[] WAVEFORM_TIMINGS = new long[]{500, 500};
+    private static final int[] WAVEFORM_AMPLITUDES = new int[]{128, 255};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Vibrator vibrator = getSystemService(Vibrator.class);
+        findViewById(R.id.click_effect_button).setOnClickListener(v -> {
+            Snackbar.make(v, "android.os.VibrationEffect.EFFECT_CLICKは非公開APIです", Snackbar.LENGTH_SHORT).show();
+            // vibrator.vibrate(VibrationEffect.createPredefined(EFFECT_CLICK));
+        });
+
+        findViewById(R.id.oneshot_effect_button).setOnClickListener(v -> {
+            vibrator.vibrate(VibrationEffect.createOneShot(ONE_SHOT_TIMING, ONE_SHOT_AMPLITUDE));
+        });
+
+        final Button waveformEffectButton = findViewById(R.id.waveform_effect_button);
+        waveformEffectButton.setOnClickListener(v -> {
+            vibrator.vibrate(VibrationEffect.createWaveform(WAVEFORM_TIMINGS, WAVEFORM_AMPLITUDES, -1));
+
+            if (vibrator.hasAmplitudeControl()) {
+                waveformEffectButton.setText(getString(R.string.button_3_pass));
+                waveformEffectButton.setBackgroundColor(Color.GREEN);
+                waveformEffectButton.setTextColor(Color.BLACK);
+            } else {
+                waveformEffectButton.setText(getString(R.string.button_3_fail));
+                waveformEffectButton.setBackgroundColor(Color.RED);
+                waveformEffectButton.setTextColor(Color.WHITE);
+            }
+        });
     }
 }
